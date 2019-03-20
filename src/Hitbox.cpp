d58b71a6aca6c674ca63359ad65a8686736a51eb
Camilo Rosales <camilomarraui@gmail.com>
2019-03-20 15:20:36 -0500

    Hitbox now has a solid border and a transparent body

--- src/Hitbox.cpp
+++ src/Hitbox.cpp
@@ -9,6 +9,12 @@ Hitbox::~Hitbox(){
 }
 
 void Hitbox::render( SDL_Renderer* renderer ){
-    SDL_SetRenderDrawColor( renderer, 0xFF, 0x00, 0x00, 0xFF );
+    //Draw transparent body
+    SDL_SetRenderDrawColor( renderer, 0xFF, 0x00, 0x00, 0x60 );
     SDL_RenderFillRect( renderer, &mRect );
+
+    //Draw solid border
+    SDL_SetRenderDrawColor( renderer, 0xFF, 0x00, 0x00, 0xFF );
+    SDL_RenderDrawRect( renderer, &mRect );
+
 }
